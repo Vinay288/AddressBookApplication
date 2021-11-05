05d3d4548607d63c0c45e2bc45f3cfeb78d50f2c
Vinay288 <vinayhiremath288@gmail.com>
2021-11-06 00:34:07 +0530

    ADDED: validation for contact attributes

--- src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
@@ -1,14 +1,35 @@
 package com.bridgelabz.addressbook.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 
 public @Data class ContactDTO {
+	@NotNull
+	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$", message = "first name Invalid")
 	public String firstName;
+
+	@NotNull
+	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$", message = "last name Invalid")
 	public String lastName;
+
+	@NotNull
+	@Pattern(regexp = "^[+]?([0-9]{2})?[789]{1}[0-9]{9}$", message = "PhoneNumber Invalid")
 	public String phoneNumber;
+
+	@NotNull
 	public String city;
+
+	@NotNull
 	public String state;
+
+	@NotNull
+	@Pattern(regexp = "^[0-9]{3}\\s?[0-9]{3}$", message = "Zip code invalid")
 	public String zipCode;
+
+	@NotNull
+	@Pattern(regexp = "^[A-Za-z0-9]+[_+.-]?[a-zA-Z0-9]*[@][A-Za-z]+[.][a-zA-Z]*[.]?[a-zA-Z]+$", message = "Email invalid")
 	public String email;
 
 	public ContactDTO(String firstName, String lastName, String phoneNumber, String city, String state, String zipCode,
