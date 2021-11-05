362fbb44ae0ee8d6ba73ab93dcb584ca4d04ed57
Vinay288 <vinayhiremath288@gmail.com>
2021-11-05 20:20:42 +0530

    ADDED: http get method

---
+++ src/main/java/com/bridgelabz/addressbook/controller/AddressBookController.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.addressbook.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddressBookController {
+	Map<Long, String> addressBook = new HashMap<>();
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping(value = { "", "/", "/get" })
+	public ResponseEntity<String> getAddressBookData() {
+		return new ResponseEntity<String>(addressBook.values().toString(), HttpStatus.OK);
+	}
+
+}
