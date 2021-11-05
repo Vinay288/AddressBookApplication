b905a20763e653df7dc32937c86a4ac2da66a2dc
Vinay288 <vinayhiremath288@gmail.com>
2021-11-05 22:24:06 +0530

    ADDED: addressbook service interface

---
+++ src/main/java/com/bridgelabz/addressbook/service/IAddressBookService.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbook.service;
+
+import java.util.List;
+
+import com.bridgelabz.addressbook.dto.ContactDTO;
+import com.bridgelabz.addressbook.model.ContactData;
+
+public interface IAddressBookService {
+	 List<ContactData> getContact();
+	    ContactData getContactById(int contactId);
+	    ContactData createContact(ContactDTO contactDTO);
+	    ContactData updateContact(int contactId,ContactDTO contactDTO);
+	    void deleteContact(int contactId);
+}
