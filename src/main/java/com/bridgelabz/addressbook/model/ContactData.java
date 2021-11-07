a17f5e5751fb2beb2703760f613c0cc8a73ba78c
Vinay288 <vinayhiremath288@gmail.com>
2021-11-07 21:14:07 +0530

    ADDED: entity annotation to match model with database

--- src/main/java/com/bridgelabz/addressbook/model/ContactData.java
+++ src/main/java/com/bridgelabz/addressbook/model/ContactData.java
@@ -1,17 +1,40 @@
 package com.bridgelabz.addressbook.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.bridgelabz.addressbook.dto.ContactDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "contact")
 public @Data class ContactData {
+	@Id
+	@Column(name = "contact_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int contactId;
+
+	@Column(name = "first_name")
 	private String firstName;
+
+	@Column(name = "last_name")
 	private String lastName;
+
+	@Column(name = "phone_number")
 	private String phoneNumber;
+
 	private String city;
+
 	private String state;
+
+	@Column(name = "zip_code")
 	private String zipCode;
+
 	private String email;
 
 	public ContactData() {
