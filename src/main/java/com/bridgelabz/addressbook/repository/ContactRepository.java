1dfc2a85a18876747507e83553aabe1e05a38232
Vinay288 <vinayhiremath288@gmail.com>
2021-11-09 00:42:48 +0530

    ADDED: method to get conatct by state

--- src/main/java/com/bridgelabz/addressbook/repository/ContactRepository.java
+++ src/main/java/com/bridgelabz/addressbook/repository/ContactRepository.java
@@ -1,11 +1,16 @@
 package com.bridgelabz.addressbook.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bridgelabz.addressbook.model.ContactData;
 
 @Repository
 public interface ContactRepository extends JpaRepository<ContactData, Integer>{
+@Query(value="select * from contact where state = :state",nativeQuery = true)
+List<ContactData> findContactByState(String state);
 
 }
